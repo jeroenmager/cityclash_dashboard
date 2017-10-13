a0588cba16299c24b281812cf06eb04bf651888f
rickb <rickb@DESKTOP-7AUNTN5.fritz.box>
2017-10-13 10:15:46 +0200

    Removed test.php file

--- app/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$json = file_get_contents("http://cityclash.icthardenberg.nl/dev/app/data/questions.php?type=maps");
-$json_array = json_decode($json);
-
-foreach($json_array as $key => $value) {
-    echo $value->text . ", " . $value->type . "<br>";
-  }
